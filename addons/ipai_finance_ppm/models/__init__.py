85a3921cf8bcf6cf252fe32a7e7ca72fa7314979
Jake Tolentino <jgtolentino_rn@yahoo.com>
2025-12-17 21:10:03 +0800

    fix(finance-ppm): Add missing project_task import to enable generator tracking fields
    The project_task.py model extension was never being loaded because it wasn't
    imported in models/__init__.py. This prevented the 6 generator tracking fields
    (x_cycle_key, x_task_template_code, x_step_code, x_external_key, x_seed_hash,
    x_obsolete) from being added to the project.task table.

--- addons/ipai_finance_ppm/models/__init__.py
+++ addons/ipai_finance_ppm/models/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from . import finance_person
 from . import finance_task
+from . import project_task
 from . import bir_schedule
 from . import ppm_dashboard
 from . import closing_models
