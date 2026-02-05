662a05b1b07b4e103a402cb52557c624a70f4e1d
jgtolentino <jgtolentino_rn@yahoo.com>
2026-02-05 17:27:03 +0800

    chore(repo): refactor to supabase-style monorepo layout (runtime/addons/vendor/infra) (#320)

--- addons/oca/account_financial_report/__init__.py
+++
@@ -1,4 +0,0 @@
-# Copyright 2024-2026 InsightPulse AI
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from . import models
--- addons/oca/hr_expense/wizard/__init__.py
+++
@@ -1,4 +0,0 @@
-# Copyright 2024 IPAI - InsightPulse AI
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-
-from . import hr_expense_refuse_reason
